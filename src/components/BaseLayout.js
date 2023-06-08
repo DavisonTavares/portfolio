46e39ef15e084d7af1a423850bf6748937bd99e6
davison <12345davison12345@gmail.com>
2023-06-08 15:33:57 -0300

    apontando a homepage para portifolio/

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -23,9 +23,10 @@ export default function BaseLayout() {
             </Grid>
             <Grid item flexGrow={1}>
                <Routes>
-                  <Route exact path={'*'} element={<Home/>}/>
+                  <Route exact path={'/'} element={<Home/>}/>
                   <Route exact path={'/about'} element={<About/>}/>
                   <Route exact path={'/portfolio'} element={<Portfolio/>}/>
+                  <Route exact path={'/portifolio'} element={<Home/>}/>
                </Routes>
             </Grid>
          </Grid>
